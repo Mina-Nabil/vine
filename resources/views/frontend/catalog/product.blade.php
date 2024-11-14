5faaa1aabeaad8607712370f0d6933d463c8df9b
Mina Nabil <mina9492@hotmail.com>
2024-11-14 12:00:16 +0200

    working on vine

--- resources/views/frontend/catalog/product.blade.php
+++ resources/views/frontend/catalog/product.blade.php
@@ -126,11 +126,11 @@
                             </figure>
                             <div class="ws-works-caption text-center">
                                 <!-- Item Category -->
-                                <div class="ws-item-category">{{ $prod->subcategory->category->name }} -
-                                    {{ $prod->subcategory->name }}</div>
+                                <div class="ws-item-category">{{ $prod->subcategory->category->arabic_name }} -
+                                    {{ $prod->subcategory->arabic_name }}</div>
 
                                 <!-- Title -->
-                                <h3 class="ws-item-title">{{ $prod->name }}</h3>
+                                <h3 class="ws-item-title">{{ $prod->arabic_name }}</h3>
 
                                 <div class="ws-item-separator"></div>
 
