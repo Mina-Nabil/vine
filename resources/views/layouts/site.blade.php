ec494df972e82c40457330ac787cdc68c6f02b4b
Mina Nabil <mina9492@hotmail.com>
2025-03-08 18:17:45 +0200

    fixing total

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -242,13 +242,15 @@
                     <!-- Instagram Information -->
                     <div class="col-sm-3">
                         <div class="ws-instagram-header">
+                            <a href="https://wa.me/+201272537888" target="_blank" class="ws-instagram-link">
+                                <h3>Whatsapp</h3>
+                            </a>
                             <h3>Instagram</h3>
                             <br>
-                            <a href="https://wa.me/+201272537888" target="_blank"
-                                class="ws-instagram-link"> Whatsapp</a> <br /> <br />
+
                             <a href="https://instagram.com/{{ $site_info['WBST_INST'] }}" target="_blank"
                                 class="ws-instagram-link"> {{ '@' . $site_info['WBST_INST'] }}</a>
-                                <p>Follow us on Instagram for our latest ideas and products</p>
+                            <p>Follow us on Instagram for our latest ideas and products</p>
                         </div>
                     </div>
 
