34b70d3dd7a32cfb09dce7c23b5729066629ff05
Mina Nabil <mina9492@hotmail.com>
2025-03-17 21:17:57 +0200

    adding fb pixel

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -18,6 +18,34 @@
 
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="{{ url('assets/img/favicon.ico') }}">
+
+    <!-- Meta Pixel Code -->
+    <script>
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '2050922418710174');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=2050922418710174&ev=PageView&noscript=1" /></noscript>
+    <!-- End Meta Pixel Code -->
+
     <script>
         window.dataLayer = window.dataLayer || [];
 
@@ -243,16 +271,16 @@
                     <div class="col-sm-3">
                         <div class="ws-instagram-header">
                             <h3>Instagram</h3>
-                    
+
 
                             <a href="https://instagram.com/{{ $site_info['WBST_INST'] }}" target="_blank"
                                 class="ws-instagram-link"> {{ '@' . $site_info['WBST_INST'] }}</a>
                             <p>Follow us on Instagram for our latest ideas and products</p>
                             <br>
                             <h3>Whatsapp</h3>
-                       
-                            <a href="https://wa.me/+201200165007" target="_blank"
-                                class="ws-instagram-link"> <i class="fa fa-whatsapp fa-lg"></i> +201200165007 </a>
+
+                            <a href="https://wa.me/+201200165007" target="_blank" class="ws-instagram-link"> <i
+                                    class="fa fa-whatsapp fa-lg"></i> +201200165007 </a>
                             <p>Chat with us</p>
                         </div>
                     </div>
